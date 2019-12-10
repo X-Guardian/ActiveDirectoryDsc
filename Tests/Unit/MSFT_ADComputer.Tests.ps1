7030d1998b3cc431f004ac9ad56433b70d1fd645
Simon Heather <simonheather99@gmail.com>
2019-12-10 21:45:20 +0000

    Update ADComputer.Tests

--- Tests/Unit/MSFT_ADComputer.Tests.ps1
+++ Tests/Unit/MSFT_ADComputer.Tests.ps1
@@ -1,21 +1,4 @@
-#region HEADER
-$script:projectPath = "$PSScriptRoot\..\.." | Convert-Path
-$script:projectName = (Get-ChildItem -Path "$script:projectPath\*\*.psd1" | Where-Object -FilterScript {
-        ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-        $(try { Test-ModuleManifest -Path $_.FullName -ErrorAction Stop } catch { $false })
-    }).BaseName
-
-$script:parentModule = Get-Module -Name $script:projectName -ListAvailable | Select-Object -First 1
-$script:subModulesFolder = Join-Path -Path $script:parentModule.ModuleBase -ChildPath 'Modules'
-Remove-Module -Name $script:parentModule -Force -ErrorAction 'SilentlyContinue'
-
-$script:subModuleName = (Split-Path -Path $PSCommandPath -Leaf) -replace '\.Tests.ps1'
-$script:subModuleFile = Join-Path -Path $script:subModulesFolder -ChildPath "$($script:subModuleName)/$($script:subModuleName).psm1"
-
-Import-Module $script:subModuleFile -Force -ErrorAction Stop
- #endregion HEADER
-
- Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\ActiveDirectoryDsc.TestHelper.psm1')
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\ActiveDirectoryDsc.TestHelper.psm1')
 
 if (-not (Test-RunForCITestCategory -Type 'Unit' -Category 'Tests'))
 {
